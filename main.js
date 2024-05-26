bb315208811ec9d06c2e98f872d5517af0304ddd
kifatet-n <n.kifayet17@gmail.com>
2024-05-26 18:53:20 +0400

    chance

--- main.js
+++ main.js
@@ -1,5 +1,99 @@
-let str="  Salam   "
-let trimmed=str.trimEnd();
-console.log(trimmed);
+//1
+//let str1="Home";
+//let str2="Work";
+//console.log(str1+str2);
+
+//2
+//let a=5;
+//let b=7;
+//console.log(a+b);
+
+//3
+//let str="homework";
+//console.log(str.lenght);
+
+//4
+// let str="homework";
+// console.log(str.toUpperCase());
+
+//5
+//let num="5";
+//console.log(typeof num);
+
+//6
+// let num = 5;
+// if (num >= 0) {
+//     alert("eded musbetdir")
+// }
+//  else {
+//     alert("eded menfidir")
+// }
+
+
+//7
+// let str ="home work";
+// let hasword = str.includes("work")
+// console.log(hasword);
+
+//8
+// let string1="HOME"
+// let string2="WORK"
+// console.log(string1===string2);
+
+//9
+//let str="homework";
+//let sliced=str.slice(2,6);
+//console.log(sliced);
+
+//10
+//let a=6;
+//let b=5;
+//let product=a*b;
+//console.log(product);
+
+//11
+//let a=15;
+//let b=4;
+//let remainder=a%b;
+//console.log(remainder);
+
+//12
+//let str1="home work";
+//let str2="work home";
+//let starts=str2.startsWith(str1);
+//console.log(starts);
+
+//13
+//let str1="home work";
+//let str2="work home";
+//let ends=str2.endsWith(str1);
+//console.log(ends);
+
+//14
+// let a = 5;
+//  let b = 7;
+// console.log(a != b); 
+
+
+
+let a = 6;
+let ischeck = (a>0) ? "musbetdir" : "menfidir"
+console.log(ischeck)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
